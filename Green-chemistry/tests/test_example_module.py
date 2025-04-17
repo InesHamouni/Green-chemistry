33845cd4af13456c5264014c0a80de2c369f1a34
InesHamouni <inesh7426@yahoo.com>
2025-04-17 17:43:26 +0200

    Initial commit avec template

---
+++ Green-chemistry/tests/test_example_module.py
@@ -0,0 +1,6 @@
+from green_chemistry.example_module import hello_smiles
+
+
+# Test the function
+def test_hello_smiles():
+    assert hello_smiles("C(=O)O") == "Hello, C(=O)O!", "Test failed: SMILES input"
